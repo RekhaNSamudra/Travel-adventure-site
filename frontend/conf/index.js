86b59b69148261f145a4e123582269952f5ee33e
CrioUser <crio-user@criodo.com>
2022-10-18 23:40:36 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURES

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://43.204.158.114:8082" };
+const config = { backendEndpoint: "http://43.204.136.191:8082" };
 
 export default config;
--- frontend/modules/adventures_page.js
+++ frontend/modules/adventures_page.js
@@ -5,21 +5,51 @@ import config from "../conf/index.js";
 function getCityFromURL(search) {
   // TODO: MODULE_ADVENTURES
   // 1. Extract the city id from the URL's Query Param and return it
-
+  const cityId = new URLSearchParams(search);
+  return cityId.get('city');
 }
 
 //Implementation of fetch call with a paramterized input based on city
 async function fetchAdventures(city) {
   // TODO: MODULE_ADVENTURES
   // 1. Fetch adventures using the Backend API and return the data
-
-}
+  try {
+    const adventures = await fetch(config.backendEndpoint + `/adventures?city=${city}`);
+    return adventures.json();
+  } catch (err) {
+    return null;
+  }
+} 
 
 //Implementation of DOM manipulation to add adventures for the given city from list of adventures
 function addAdventureToDOM(adventures) {
   // TODO: MODULE_ADVENTURES
   // 1. Populate the Adventure Cards and insert those details into the DOM
-
+   let parent = document.getElementById("data");
+   adventures.forEach(function(card){
+   let adventuresDiv = document.createElement("div");
+    adventuresDiv.classList.add("col-12", "col-sm-6", "col-lg-3", "mb-3");
+    adventuresDiv.innerHTML = `
+    <a href="detail/?adventure=${card.id}" id="${card.id}">
+    <div class="activity-card">
+    <img src=${card.image}> 
+    <div class="category-banner">${card.category}
+    </div>
+    <div class="card-body col-md-12 mt-2">
+    <div class="d-flex justify-content-between">
+    <p>${card.name}</p>
+    <p>${card.costPerHead}</p>
+    </div>
+    <div class="d-flex justify-content-between">
+    <p>Duration</p>
+    <P>${card.duration} Hours</p>
+    </div>
+    </div>
+    </div>
+    </a>
+    `
+    parent.appendChild(adventuresDiv);
+   }) 
 }
 
 //Implementation of filtering by duration which takes in a list of adventures, the lower bound and upper bound of duration and returns a filtered list of adventures.
