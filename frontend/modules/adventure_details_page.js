95a1be31282ef01686c3c0567aa37b9fa8e0896c
CrioUser <crio-user@criodo.com>
2022-11-09 13:49:17 +0530

    ME_QTRIPDYNAMIC_MODULE_RESERVATIONS

--- frontend/modules/adventure_details_page.js
+++ frontend/modules/adventure_details_page.js
@@ -56,55 +56,67 @@ function addAdventureDetailsToDOM(adventure) {
 function addBootstrapPhotoGallery(images) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the bootstrap carousel to show the Adventure images
-  document.getElementById("photo-gallery").innerHTML = `
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#arouselExampleIndicators" data-bs-slide="0"  class="active" aria-current="true" aria-label="Slide 1"</li>
-      <button type="button" data-bs-target="#arouselExampleIndicators" data-bs-slide="1"  aria-label="Slide 2"</li>
-      <button type="button" data-bs-target="#arouselExampleIndicators" data-bs-slide="2"  aria-label="Slide 3"</li>
-    </div>
 
-    <div class="carousel-inner" id="carousel-inner">
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Prev</span>
+document.getElementById("photo-gallery").innerHTML = `
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner" id="carousel-inner">
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
   </button>
-</div>`;
-
-images.map((image, idx) => {
-  let ele = document.createElement("div");
-  ele.className = `carousel-item ${idx === 0 ? "active" : ""}`;
-  ele.innerHTML =  `
-  <img 
-      src=${image}
-      alt=""
-      srcset=""
-      class="activity-card-image pb-3 pb-md-0"
-      />
+  </div>
   `;
 
-  document.getElementById("carousel-inner").appendChild(ele);
-});
+  let inner = document.getElementById("carousel-inner");
 
+  for (let i = 0; i < images.length; i++) {
+    let div = document.createElement("div");
+    if (i === 0) {
+      div.className = "carousel-item active";
+    }
+    else {
+      div.className = "carousel-item";
+    }
+    div.innerHTML = `
+    <img src="${images[i]}">
+    `;
+    // console.log(div)
+    inner.append(div);
+  }
 }
+
+  
+
 
 //Implementation of conditional rendering of DOM based on availability
 function conditionalRenderingOfReservationPanel(adventure) {
   // TODO: MODULE_RESERVATIONS
   // 1. If the adventure is already reserved, display the sold-out message.
-
+  if(adventure.available) {
+    document.getElementById("reservation-panel-available").style.display = "block";
+    document.getElementById("reservation-panel-sold-out").style.display = "none";
+    document.getElementById("reservation-person-cost").innerHTML = adventure.costPerHead;
+  } else {
+    document.getElementById("reservation-panel-sold-out").style.display = "block";
+    document.getElementById("reservation-panel-available").style.display = "none";
+  }
 }
 
 //Implementation of reservation cost calculation based on persons
 function calculateReservationCostAndUpdateDOM(adventure, persons) {
   // TODO: MODULE_RESERVATIONS
   // 1. Calculate the cost based on number of persons and update the reservation-cost field
-
+  document.getElementById("reservation-cost").innerHTML = adventure.costPerHead * persons;
 }
 
 //Implementation of reservation form submission
@@ -112,13 +124,51 @@ function captureFormSubmit(adventure) {
   // TODO: MODULE_RESERVATIONS
   // 1. Capture the query details and make a POST API call using fetch() to make the reservation
   // 2. If the reservation is successful, show an alert with "Success!" and refresh the page. If the reservation fails, just show an alert with "Failed!".
+  const form = document.getElementById("myForm");
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    let url = config.backendEndpoint + "/reservations/new";
+    let formElements = form.elements;
+    let bodyString = JSON.stringify({
+      name: formElements["name"].value,
+      date: formElements["date"].value,
+      person: formElements["person"].value,
+      adventure: adventure.id,
+    });
+
+    try {
+      let res = await fetch(url, {
+        method: "POST",
+        body: bodyString,
+        headers: {
+          "content-Type": "application/json",
+        },
+      });
+
+      debugger;
+      if(res.ok) {
+        alert("Success!");
+        window.location.reload();
+      } else {
+        let data = await res.json();
+        alert(`Failed - ${data.message}`);
+      }
+    } catch (err) {
+      console.log(err);
+      alert("Failed - fetch call resulted in error");
+    }
+  });
 }
 
 //Implementation of success banner after reservation
 function showBannerIfAlreadyReserved(adventure) {
   // TODO: MODULE_RESERVATIONS
   // 1. If user has already reserved this adventure, show the reserved-banner, else don't
-
+  if(adventure.reserved) {
+    document.getElementById("reserved-banner").style.display = "block";
+  } else {
+    document.getElementById("reserved-banner").style.display = "none";
+  }
 }
 
 export {
